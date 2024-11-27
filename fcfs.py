01a3850cb098a153cde405349a7777f7d628fc6c
RandomShtuf <79044047+RandomShtuf@users.noreply.github.com>
2024-11-28 02:52:23 +0800

    added executing status
    marks running processes as executing

--- fcfs.py
+++ fcfs.py
@@ -1,16 +1,30 @@
-from operating_systems import ProcessTable, ReadyQueue
+from operating_systems import ProcessTable, ReadyQueue, ExecutingQueue
+from time import sleep
+from os import system
 
 #processsCount = int(input("Enter the number of processes: "))
 process_table = ProcessTable()
 ready_queue = ReadyQueue()
+executing_queue = ExecutingQueue()
 
 def handleArrivingProcess(unit_time):
     for process in process_table.getTable():
         if process["arrival_time"] == unit_time:
             process_table.updateProcessStatus(process["process_num"], "ready")
             ready_queue.enqueue(process["process_num"], process["arrival_time"], process["burst_time"])
-    process_table.printTable()
-    ready_queue.printQueue()
+    
+def executeReadyProcess():
+    temp = ready_queue.dequeue()
+    executing_queue.enqueue(temp)
+    process_table.updateProcessStatus(temp.getProcessNum(), "executing")
+        
+def handleExecutingProcess():
+    burst_time = executing_queue.peek().getBurstTime()
+    if burst_time == 0:
+        temp = executing_queue.dequeue()
+        process_table.updateProcessStatus(temp.getProcessNum(), "terminated")
+        return
+    executing_queue.peek().setBurstTime(burst_time - 1)
 
 
 #for i in range(processsCount):
@@ -22,9 +36,20 @@ process_table.addProcess(1, 2, 4)
 process_table.addProcess(2, 5, 6)
     
 process_table.printTable()
+input("Press Enter To Continue: ")
 
 unit_time = 0
 while not process_table.isAllProcessComplete():
+    system("cls")
+    print(f"Unit Time: {unit_time}")
+    if not executing_queue.isEmpty():
+        handleExecutingProcess()
     handleArrivingProcess(unit_time)
+    if executing_queue.isEmpty() and not ready_queue.isEmpty():
+        executeReadyProcess()
+    process_table.printTable()
+    executing_queue.printQueue("Executing")
+    ready_queue.printQueue("Ready")
         
     unit_time += 1
+    sleep(2)
--- operating_systems.py
+++ operating_systems.py
@@ -8,9 +8,9 @@ class Process:
         return self.__process_num
     def getArrivalTime(self):
         return self.__arrival_time
-    def getBurst_time(self):
+    def getBurstTime(self):
         return self.__burst_time
-    def setBurst_time(self, burst_time):
+    def setBurstTime(self, burst_time):
         self.__burst_time = burst_time
     
 class ProcessTable:
@@ -38,7 +38,7 @@ class ProcessTable:
     def updateProcessStatus(self, process_num, status):
         for process in self.__table:
             if process["process_num"] == process_num:
-                process["status"] = "ready"
+                process["status"] = status
                 break
         
     def printTable(self):
@@ -49,27 +49,36 @@ class ProcessTable:
     def isAllProcessComplete(self):
         not_terminated = 0
         for process in self.__table:
-            #if process["status"] != "terminated": not_terminated += 1
-            if process["status"] != "ready": not_terminated += 1
+            if process["status"] != "terminated": not_terminated += 1
             
         return  not_terminated == 0
-            
-class ReadyQueue:
+    
+class Queue:
     def __init__(self):
-        self.__ready_queue = []
+        self._queue = []
         
-    def enqueue(self, process_num, arrival_time, burst_time):
-        ready_process = Process(process_num, arrival_time, burst_time)
-        self.__ready_queue.append(ready_process)
+    def peek(self):
+        return self._queue[0]
+        
+    def enqueue(self, process):
+        self._queue.append(process)
         
     def dequeue(self):
-        self.__ready_queue.pop(0)
+        return self._queue.pop(0)
         
     def isEmpty(self):
-        return len(self.__ready_queue) == 0
-        
-    def printQueue(self):
-        print("Ready: [", end="")
-        for ready_process in self.__ready_queue:
-            print(f" p{ready_process.getProcessNum()} ", end="")
+        return len(self._queue) == 0
+    
+    def printQueue(self, queue_name = "Queue"):
+        print(f"{queue_name}: [", end="")
+        for process in self._queue:
+            print(f" p{process.getProcessNum()} ", end="")
         print("]")
+            
+class ReadyQueue(Queue):
+    def enqueue(self, process_num, arrival_time, burst_time):
+        process = Process(process_num, arrival_time, burst_time)
+        self._queue.append(process)
+        
+class ExecutingQueue(Queue):
+    pass

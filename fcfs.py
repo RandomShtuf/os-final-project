de2f9299696e264db6bd34169fe3d0b65bedd34b
RandomShtuf <79044047+RandomShtuf@users.noreply.github.com>
2024-11-28 01:31:14 +0800

    added ariving process handler
    enqueues arriving processes to ReadyQueue and sets their status to ready

--- fcfs.py
+++ fcfs.py
@@ -1,8 +1,17 @@
-from operating_systems import ProcessTable
-
+from operating_systems import ProcessTable, ReadyQueue
 
 #processsCount = int(input("Enter the number of processes: "))
 process_table = ProcessTable()
+ready_queue = ReadyQueue()
+
+def handleArrivingProcess(unit_time):
+    for process in process_table.getTable():
+        if process["arrival_time"] == unit_time:
+            process_table.updateProcessStatus(process["process_num"], "ready")
+            ready_queue.enqueue(process["process_num"], process["arrival_time"], process["burst_time"])
+    process_table.printTable()
+    ready_queue.printQueue()
+
 
 #for i in range(processsCount):
 #    arrival_time = int(input(f"Enter arrival time for p{i}: "))
@@ -13,3 +22,9 @@ process_table.addProcess(1, 2, 4)
 process_table.addProcess(2, 5, 6)
     
 process_table.printTable()
+
+unit_time = 0
+while not process_table.isAllProcessComplete():
+    handleArrivingProcess(unit_time)
+        
+    unit_time += 1
--- operating_systems.py
+++ operating_systems.py
@@ -17,6 +17,10 @@ class ProcessTable:
     def __init__(self):
         self.__table = []
         
+    def getTable(self):
+        table = self.__table
+        return table
+        
     def addProcess(self, process_num = 0, arrival_time = 0, burst_time = 0):
         process = {
             "process_num": process_num,
@@ -31,6 +35,12 @@ class ProcessTable:
         
         self.__table.append(process)
         
+    def updateProcessStatus(self, process_num, status):
+        for process in self.__table:
+            if process["process_num"] == process_num:
+                process["status"] = "ready"
+                break
+        
     def printTable(self):
         for process in self.__table:
             for item in process.items(): print(f"p{item[1]}", end="") if item[0] == "process_num" else print(" | ", item[1], end="")
@@ -39,7 +49,8 @@ class ProcessTable:
     def isAllProcessComplete(self):
         not_terminated = 0
         for process in self.__table:
-            if process["status"] != "terminated": not_terminated += 1
+            #if process["status"] != "terminated": not_terminated += 1
+            if process["status"] != "ready": not_terminated += 1
             
         return  not_terminated == 0
             
@@ -48,14 +59,17 @@ class ReadyQueue:
         self.__ready_queue = []
         
     def enqueue(self, process_num, arrival_time, burst_time):
-        process = Process(process_num, arrival_time, burst_time)
-        self.__ready_queue.append(process)
+        ready_process = Process(process_num, arrival_time, burst_time)
+        self.__ready_queue.append(ready_process)
         
-    def dequeue(self, process_num, arrival_time, burst_time):
+    def dequeue(self):
         self.__ready_queue.pop(0)
+        
+    def isEmpty(self):
+        return len(self.__ready_queue) == 0
         
     def printQueue(self):
         print("Ready: [", end="")
-        for process in ReadyQueue:
-            print(f" {process.getProcessNum()} ", end="")
+        for ready_process in self.__ready_queue:
+            print(f" p{ready_process.getProcessNum()} ", end="")
         print("]")
